5b09dc36b39e761c49f18f64a94c4b4bc91031d1
Clare McNamara <clarem@Clares-MacBook-Air-2.local>
2022-12-06 11:20:11 -0500

    updates

--- covid_by_day.py
+++ covid_by_day.py
@@ -105,7 +105,7 @@ def add_data_to_table(big_data_dict, cur, conn):
 
 # # TASK 2: GET TEMP AND CASES INFO JOINED
 def connect_temp_and_covid_by_date(cur, conn):
-    cur.execute('SELECT Temperature.date, Temperature.state, Temperature.temp, Covid.date, Covid.state, Covid.cases FROM Covid JOIN Temperature ON Temp.date = Covid.date')
+    cur.execute('SELECT Temperature.date, Temperature.state, Temperature.avg_temp, Covid.date, Covid.state, Covid.cases FROM Covid JOIN Temperature ON Temp.date = Covid.date')
     list_of_matches = cur.fetchall()
     return list_of_matches
 
